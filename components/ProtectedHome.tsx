145820f737dedfdbc53fd129f80016c87415bfbd
eyob13-coder <eyobgeremew618@gmail.com>
2025-06-27 14:09:47 +0300

    second phase

--- components/ProtectedHome.tsx
+++ components/ProtectedHome.tsx
@@ -7,6 +7,7 @@ import CTA from "@/components/CTA";
 import { getAllCompanions } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 
+
 export default function ProtectedHome() {
   const { isSignedIn } = useUser();
   const [companions, setCompanions] = useState<any[]>([]);
@@ -20,25 +21,28 @@ export default function ProtectedHome() {
   if (!isSignedIn) return null;
 
   return (
-    <>
-      <h1>Popular Companions</h1>
+    <main>
+    <h1>Popular Companions</h1>
+
       <section className="home-section">
-        {companions.map((companion: any) => (
-          <CompanionCard
-            key={companion.id}
-            {...companion}
-            color={getSubjectColor(companion.subject)}
+          {companions.map((companion) => (
+              <CompanionCard
+                  key={companion.id}
+                  {...companion}
+                  color={getSubjectColor(companion.subject)}
+              />
+          ))}
+
+      </section>
+
+      <section className="home-section">
+          <CompanionsList
+              title="Recently completed sessions"
+              companions={[]}
+              classNames="w-2/3 max-lg:w-full"
           />
-        ))}
+          <CTA />
       </section>
-      <section className="home-section">
-        <CompanionsList
-          title="Recently completed sessions"
-          companions={[]}
-          classNames="w-2/3 max-lg:w-full"
-        />
-        <CTA />
-      </section>
-    </>
+  </main>
   );
 } 
